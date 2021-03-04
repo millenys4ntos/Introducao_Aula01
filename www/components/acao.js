b96ec0e3b8f4e49e43794f028d4f17d7fb53b056
millenys4ntos <79285121+millenys4ntos@users.noreply.github.com>
2021-03-04 17:51:35 -0300

    Update acao.js

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,38 @@
 // This is a JavaScript file
 
+$(document).on("click", "#somar", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+  
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#subtrair", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#multiplicar", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#dividir", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+})
+
